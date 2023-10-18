672d48b42bd2bb241822f052d7e2686e2a24c81c
kawser2917 <kawserahmed8492@gmail.com>
2023-10-18 20:21:05 +0600

    prompt done

--- js/prompt.js
+++ js/prompt.js
@@ -12,6 +12,15 @@ function seriousStudent(){
         alert("Soon you will face the hard world")
     }
 
- 
+}
 
+function getName(){
+    const name = prompt("What's your name?")
+
+    if(name === "kawser"){
+        alert("Kawser Please Don't give up")
+    }
+    else{
+        alert("My browser Donesn't know you")
+    }
 }
