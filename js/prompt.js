ac49c50f743576d74ced4fb3568824a622cec470
kawser2917 <kawserahmed8492@gmail.com>
2023-10-18 20:17:45 +0600

    confirm done

--- js/prompt.js
+++ js/prompt.js
@@ -1,3 +1,17 @@
 function beAttentive(){
     alert("Please be attentive before it's too late")
 }
+
+function seriousStudent(){
+    const serious = confirm("Are you spending a lot of time to learn new thing")
+
+    if(serious === true){
+        alert("Well done!! Keep it up")
+    }
+    else{
+        alert("Soon you will face the hard world")
+    }
+
+ 
+
+}
